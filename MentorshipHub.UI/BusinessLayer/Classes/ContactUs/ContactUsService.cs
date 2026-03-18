1216209e3250b46c4ccfae9e17206ca8718afcf9
Dasireddy Venkatesh <dasireddyvenkatesh@gmail.com>
2026-03-18 17:39:33 +0530

    Api Call Handler For Contact Us Page

--- MentorshipHub.UI/BusinessLayer/Classes/ContactUs/ContactUsService.cs
+++ MentorshipHub.UI/BusinessLayer/Classes/ContactUs/ContactUsService.cs
@@ -1,17 +1,25 @@
-﻿using MentorshipHub.UI.BusinessLayer.Interfaces.ContactUs;
+﻿using MentorshipHub.UI.BusinessLayer.Interfaces.APICalls;
+using MentorshipHub.UI.BusinessLayer.Interfaces.ContactUs;
 using MentorshipHub.UI.DTO;
 
 namespace MentorshipHub.UI.BusinessLayer.Classes.ContactUs
 {
     public class ContactUsService : IContactUsService
     {
-        public Task<ContactUsResponse> SendContactUsMessageAsync(ContactUsRequest request)
+        private readonly IApiClient _api;
+
+        public ContactUsService(IApiClient api)
         {
-            return Task.FromResult(new ContactUsResponse
-            {
-                Success = true,
-                Message = "Your message has been sent successfully. We will get back to you shortly."
-            });
+            _api = api;
+        }
+
+        public async Task<ContactUsResponse> SendContactUsMessageAsync(ContactUsRequest request)
+        {
+
+            var response = await _api.PostAsync<ContactUsRequest, ContactUsResponse>("api/contactus", request);
+
+            return response.Data;
+
         }
     }
 }
